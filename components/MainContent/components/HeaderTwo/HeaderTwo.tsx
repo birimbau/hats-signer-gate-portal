601464f1a25af1b0b664f33e13933d6bb02f91fa
Tiago Neto <tiagompbneto@gmail.com>
2023-07-19 17:10:11 +0100

    fix build

--- components/MainContent/components/HeaderTwo/HeaderTwo.tsx
+++ components/MainContent/components/HeaderTwo/HeaderTwo.tsx
@@ -1,8 +1,8 @@
-import { VStack, Text } from "@chakra-ui/react";
+import { VStack, Text } from '@chakra-ui/react';
 import {
   DEPLOY_ACTIONS,
   useDeployContext,
-} from "../../../../context/DeployContext";
+} from '../../../../context/DeployContext';
 
 const HeaderTwo = () => {
   const { selectedDeployAction } = useDeployContext();
@@ -10,30 +10,30 @@ const HeaderTwo = () => {
   switch (selectedDeployAction) {
     case DEPLOY_ACTIONS.DEPLOY_HSG:
       return (
-        <VStack justifyContent="flex-end" height="100%" alignItems="flex-start">
-          <Text as="b">Deploy Hats Signer Gate</Text>
-          <Text>Input safe adrress, click 'Read'</Text>
+        <VStack justifyContent='flex-end' height='100%' alignItems='flex-start'>
+          <Text as='b'>Deploy Hats Signer Gate</Text>
+          <Text>Input safe adrress, click &apos;Read&apos;</Text>
         </VStack>
       );
     case DEPLOY_ACTIONS.DEPLOY_HSG_W_S:
       return (
-        <VStack justifyContent="flex-end" height="100%" alignItems="flex-start">
-          <Text as="b">Deploy Hats Signer Gate + Safe</Text>
-          <Text>Enter inputs, click 'Write'</Text>
+        <VStack justifyContent='flex-end' height='100%' alignItems='flex-start'>
+          <Text as='b'>Deploy Hats Signer Gate + Safe</Text>
+          <Text>Enter inputs, click &apos;Write&apos;</Text>
         </VStack>
       );
     case DEPLOY_ACTIONS.DEPLOY_MHSG:
       return (
-        <VStack justifyContent="flex-end" height="100%" alignItems="flex-start">
-          <Text as="b">Deploy Hats Signer Gate</Text>
-          <Text>Input safe adrress, click 'Read'</Text>
+        <VStack justifyContent='flex-end' height='100%' alignItems='flex-start'>
+          <Text as='b'>Deploy Hats Signer Gate</Text>
+          <Text>Input safe adrress, click &apos;Read&apos;</Text>
         </VStack>
       );
     case DEPLOY_ACTIONS.DEPLOY_MHSG_W_S:
       return (
-        <VStack justifyContent="flex-end" height="100%" alignItems="flex-start">
-          <Text as="b">Deploy Hats Signer Gate + Safe</Text>
-          <Text>Enter inputs, click 'Write'</Text>
+        <VStack justifyContent='flex-end' height='100%' alignItems='flex-start'>
+          <Text as='b'>Deploy Hats Signer Gate + Safe</Text>
+          <Text>Enter inputs, click &apos;Write&apos;</Text>
         </VStack>
       );
     default:
