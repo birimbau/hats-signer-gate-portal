820bd79edd395576eda3dedfcb3aa11f6eb39f84
Tiago Neto <tiagompbneto@gmail.com>
2023-08-20 11:08:45 +0100

    fixed build

--- hooks/useGetLayout/useGetLayout.tsx
+++ hooks/useGetLayout/useGetLayout.tsx
@@ -1,27 +1,15 @@
-'use-client';
-import { DEPLOY_ACTIONS } from '../../context/DeployContext';
-import { HEADER_ACTIONS } from '../../context/SelectedActionContext';
 import { components } from './components/components';
-import { ComponentP } from './types';
 
-const useGetLayout = ({
-  slug1,
-  slug2,
-}: {
-  slug1: HEADER_ACTIONS;
-  slug2?: DEPLOY_ACTIONS;
-}) => {
-  const component: ComponentP = slug2
-    ? components[slug1][slug2]
-    : components[slug1];
+const useGetLayout = ({ slug1, slug2 }: { slug1: string; slug2?: string }) => {
+  const component = slug2 ? components[`${slug1}_${slug2}`] : components[slug1];
 
   return {
-    headerOne: component?.headerOne,
-    headerTwo: component?.headerTwo,
-    headerThree: component?.headerThree,
-    contentOne: component?.contentOne,
-    contentTwo: component?.contentTwo,
-    contentThree: component?.contentThree,
+    headerOne: component?.().headerOne,
+    headerTwo: component?.().headerTwo,
+    headerThree: component?.().headerThree,
+    contentOne: component?.().contentOne,
+    contentTwo: component?.().contentTwo,
+    contentThree: component?.().contentThree,
   };
 };
 
