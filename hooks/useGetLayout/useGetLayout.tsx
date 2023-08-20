2d5520243e2acb846152188d382547e859fad36a
Tiago Neto <tiagompbneto@gmail.com>
2023-08-20 11:19:00 +0100

    fix build

--- hooks/useGetLayout/useGetLayout.tsx
+++ hooks/useGetLayout/useGetLayout.tsx
@@ -1,7 +1,11 @@
 import { components } from './components/components';
 
-const useGetLayout = ({ slug1, slug2 }: { slug1: string; slug2?: string }) => {
-  const component = slug2 ? components[`${slug1}_${slug2}`] : components[slug1];
+const useGetLayout = ({ slug1, slug2 }: { slug1?: string; slug2?: string }) => {
+  const component = slug2
+    ? components[`${slug1}_${slug2}`]
+    : slug1
+    ? components[slug1]
+    : undefined;
 
   return {
     headerOne: component?.().headerOne,
