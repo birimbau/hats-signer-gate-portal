5dd4174f1daa373245540b9dd0ef4801abc56449
Brayve-Pancake <josh.c.bray@gmail.com>
2023-09-20 16:31:29 -0500

    Add new inport for readForm

--- components/Deployers/forms/utils/ReadForm.tsx
+++ components/Deployers/forms/utils/ReadForm.tsx
@@ -1,37 +1,63 @@
-import { Flex, VStack } from '@chakra-ui/react';
+import { Flex, VStack, useToast } from '@chakra-ui/react';
+import { useEffect } from 'react';
 import { AiOutlineRead } from 'react-icons/ai';
 import Button from '../../../UI/CustomButton/CustomButton';
-import { Formik, Form, Field } from 'formik';
-import Input from '../../../UI/CustomInput/CustomInput';
-import { useCanAttachMHSG2Safe } from '../../../../utils/hooks/HatsSignerGateFactory';
-import { AbiTypeToPrimitiveType } from 'abitype';
+import { Formik, Form } from 'formik';
+import { AbiTypeToPrimitiveType } from 'abitype'; // Make sure you're using this import correctly.
 import CustomInputWrapper from './CustomInputWrapper';
 
+// ethers imports
+import { Contract, Provider, constant } from 'ethers';
+
+// Gnosis Safe ABI
+import { SAFE_ABI } from '../../../../utils/contracts';
+
 const ReadForm = () => {
+  const toast = useToast();
+  const alchemyProvider = new ethers.providers.AlchemyProvider(
+    'mainnet',
+    YOUR_ALCHEMY_API_KEY
+  );
+  const signer = provider.getSigner();
+  const SENTINEL_MODULES = constants.AddressZero; // This value is usually the zero address, adjust if needed
+
   const initialValues = {
-    _mhsg: '0x1195634e628aFD98d4A1A77acAB73657895E7a4C',
+    _mhsg: '',
   };
 
-  const { data, refetch } = useCanAttachMHSG2Safe(initialValues._mhsg);
+  const checkIfCanAttachHSG = async (safeAddress: string) => {
+    const safeContract = new Contract(safeAddress, SAFE_ABI, signer);
+    const modules = await safeContract.getModulesPaginated(SENTINEL_MODULES, 1);
 
-  useEffect(() => {
-    const he = data;
-    debugger;
-  }, [data]);
+    if (modules.length > 0) {
+      toast({
+        title: 'HSG cannot be attached',
+        description: 'HSG must be the sole module for a given Safe.',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      });
+      return false;
+    }
+    return true;
+  };
 
   return (
     <Formik
       initialValues={initialValues}
-      onSubmit={(values) => {
-        refetch?.();
+      onSubmit={async (values) => {
+        const canAttach = await checkIfCanAttachHSG(values._mhsg);
+        if (canAttach) {
+          // Implement what should happen when HSG can be attached
+        }
       }}
     >
-      {({ handleChange, values }) => (
+      {() => (
         <Form noValidate>
           <VStack width="100%" alignItems={'flex-start'} fontSize={14} gap={5}>
             <Flex flexDirection={'column'} gap={0} w={'100%'}>
               <CustomInputWrapper
-                name="_ownerHatId"
+                name="_mhsg"
                 label="Existing Safe (address)"
                 placeholder="0xC8ac0000000000000000000000000000000047fe"
               />
