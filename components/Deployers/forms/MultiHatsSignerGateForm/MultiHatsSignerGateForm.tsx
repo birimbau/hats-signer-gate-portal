d88808c9a7e51551ff24de49fba1c38bb603c764
Brayve-Pancake <josh.c.bray@gmail.com>
2023-09-27 15:21:53 -0500

    Fix validation for <Multiple /> Inputs

--- components/Deployers/forms/MultiHatsSignerGateForm/MultiHatsSignerGateForm.tsx
+++ components/Deployers/forms/MultiHatsSignerGateForm/MultiHatsSignerGateForm.tsx
@@ -21,6 +21,8 @@ import { HatsSignerGateFactoryAbi } from '../../../../utils/abi/HatsSignerGateFa
 import { Form, Formik } from 'formik';
 import CustomInputWrapper from '../utils/CustomInputWrapper';
 
+// TODO - create a custom component that can be used by all 4 components.
+
 interface Props {
   setIsPending: (isPending: boolean) => void;
   setData: (data: any) => void;
@@ -217,95 +219,3 @@ export default function MultiHatsSignerGateForm(props: Props) {
     </Formik>
   );
 }
-// return
-// <form onSubmit={onSubmit} noValidate>
-//   <VStack width="100%" alignItems={'flex-start'} fontSize={14} gap={5}>
-//     <Input
-//       label="Owner Hat ID (integer)"
-//       placeholder="26950000000000000000000000004196..."
-//       name="_ownerHatId"
-//       value={formData._ownerHatId}
-//       width="340px"
-//       onChange={(e) =>
-//         setFormData({ ...formData, _ownerHatId: e.target.value })
-//       }
-//       isDisabled={isLoading}
-//     />
-//     <MultiInput
-//       values={formData._signersHatIds}
-//       width="372px"
-//       label="Signer Hat IDs"
-//       name="_signersHatIds"
-//       countLabel="Id"
-//       placeholder="26960000000000000000000000003152..."
-//       onChange={(_value, index, e) => {
-//         setFormData({
-//           ...formData,
-//           _signersHatIds: formData._signersHatIds.map((v, i) => {
-//             return i === index ? e.target.value : v;
-//           }),
-//         });
-//       }}
-//       onClickAdd={(value, _index) => {
-//         setFormData({
-//           ...formData,
-//           _signersHatIds: [...formData._signersHatIds, ''],
-//         });
-//       }}
-//       onClickRemove={(_value, index) => {
-//         setFormData({
-//           ...formData,
-//           _signersHatIds: formData._signersHatIds.filter(
-//             (v, i) => i !== index
-//           ),
-//         });
-//       }}
-//     />
-//     <Input
-//       label="Existing Safe (address)"
-//       placeholder="0xC8ac0000000000000000000000000000000047fe"
-//       name="_safe"
-//       value={formData._safe}
-//       width="340px"
-//       onChange={(e) => setFormData({ ...formData, _safe: e.target.value })}
-//       isDisabled={isLoading}
-//     />
-//     <Input
-//       label="Min Threshold (integer)"
-//       width="340px"
-//       placeholder="3"
-//       name="_minThreshold"
-//       value={formData._minThreshold}
-//       onChange={(e) =>
-//         setFormData({ ...formData, _minThreshold: e.target.value })
-//       }
-//       isDisabled={isLoading}
-//     />
-//     <Input
-//       label="Max Threshold (integer)"
-//       width="340px"
-//       placeholder="5"
-//       name="_targetThreshold"
-//       value={formData._targetThreshold}
-//       onChange={(e) =>
-//         setFormData({ ...formData, _targetThreshold: e.target.value })
-//       }
-//       isDisabled={isLoading}
-//     />
-//     <Input
-//       label="Max Signers (integer)"
-//       width="340px"
-//       placeholder="9"
-//       name="_maxSigners"
-//       value={formData._maxSigners}
-//       onChange={(e) =>
-//         setFormData({ ...formData, _maxSigners: e.target.value })
-//       }
-//       isDisabled={isLoading}
-//     />
-//     <Button isDisabled={isLoading} type="submit" leftIcon={<BsPen />}>
-//       Write
-//     </Button>
-//   </VStack>
-// </form>
-// );
--- components/UI/MultiInput/MultiInput.tsx
+++ components/UI/MultiInput/MultiInput.tsx
@@ -2,7 +2,6 @@ import {
   FormControl,
   FormErrorMessage,
   HStack,
-  IconButton,
   VStack,
 } from '@chakra-ui/react';
 import Input from '../CustomInput/CustomInput';
@@ -22,147 +21,86 @@ interface P {
   minAmountOfValues?: number;
 }
 
-import { FieldArray, FieldArrayRenderProps } from 'formik';
+// Changes made to this file are to enable validation to occur easily.
+// e.g. setFieldTouched needs manual updates inside of <FieldArray />
 
+import { FieldArray, FieldArrayRenderProps } from 'formik';
 const MultiInput: React.FC<P> = (p) => {
   return (
     <FieldArray name={p.name}>
       {({ push, remove, form }: FieldArrayRenderProps) => (
-        <FormControl
-          isInvalid={!!(form.errors[p.name] && form.touched[p.name])}
-        >
-          <VStack gap={'13px'} alignItems="flex-start" width={p.width}>
-            {form.values[p.name].map((v: string, i: number) => (
-              <Input
-                key={i}
-                label={`${p.label} [${p.countLabel}${i + 1}] (integer)`}
-                type={p.type || 'text'}
-                name={`${p.name}[${i}]`}
-                value={v.toString()}
-                placeholder={p.placeholder}
-                _placeholder={{
-                  fontSize: '14px',
-                }}
-                width={'100%'}
-                isDisabled={p.isDisabled}
-                onChange={(e) => {
-                  form.setFieldValue(`${p.name}[${i}]`, e.target.value);
-                }}
-                extra={
-                  <>
-                    <HStack gap="8px">
-                      {i > 0 && (
-                        <S.IconButtonStyled
-                          aria-label="remove value"
-                          icon={<HiMinus />}
-                          size="xs"
-                          isDisabled={
-                            form.values[p.name].length >=
-                              (p.minAmountOfValues || 0) || p.isDisabled
-                          }
-                          onClick={() => {
-                            remove(i);
-                          }}
-                        />
-                      )}
-                      {i === form.values[p.name].length - 1 && (
+        <VStack gap={'13px'} alignItems="flex-start" width={p.width}>
+          {form.values[p.name].map((v: string, i: number) => {
+            const isError =
+              Array.isArray(form.errors[p.name]) &&
+              typeof (form.errors[p.name] as any[])[i] === 'string';
+            const isTouched =
+              Array.isArray(form.touched[p.name]) &&
+              typeof (form.touched[p.name] as any[])[i] === 'boolean';
+
+            // console.log(`Error state for Field ${i}:`, form.errors[p.name]);
+            // console.log(`Touched state for Field ${i}:`, form.touched[p.name]);
+            // console.log(
+            //   `Combined invalid state for Field ${i}:`,
+            //   !!(isError && isTouched)
+            // );
+
+            return (
+              <FormControl key={i} isInvalid={!!(isError && isTouched)}>
+                <Input
+                  label={`${p.label} [${p.countLabel}${i + 1}] (integer)`}
+                  type={p.type || 'text'}
+                  name={`${p.name}[${i}]`}
+                  value={v.toString()}
+                  placeholder={p.placeholder}
+                  _placeholder={{
+                    fontSize: '14px',
+                  }}
+                  width={'100%'}
+                  isDisabled={p.isDisabled}
+                  onChange={(e) => {
+                    form.setFieldValue(`${p.name}[${i}]`, e.target.value);
+                    form.setFieldTouched(`${p.name}[${i}]`, true);
+                  }}
+                  onBlur={() => {
+                    form.setFieldTouched(`${p.name}[${i}]`, true);
+                  }}
+                  extra={
+                    <>
+                      <HStack gap="8px">
+                        {i > 0 && (
+                          <S.IconButtonStyled
+                            aria-label="remove value"
+                            icon={<HiMinus />}
+                            size="xs"
+                            onClick={() => {
+                              remove(i);
+                            }}
+                          />
+                        )}
                         <S.IconButtonStyled
                           aria-label="Add value"
                           icon={<BsPlusLg />}
                           size="xs"
-                          isDisabled={!form.values[p.name][i] || p.isDisabled}
+                          isDisabled={!v || p.isDisabled}
                           onClick={() => push('')}
                         />
-                      )}
-                    </HStack>
-                  </>
-                }
-              />
-            ))}
-            {/* Make sure all errors are created as strings in YupValidation */}
-            <FormErrorMessage>{form.errors[p.name] as string}</FormErrorMessage>
-          </VStack>
-        </FormControl>
+                      </HStack>
+                    </>
+                  }
+                />
+                <FormErrorMessage>
+                  {(Array.isArray(form.errors[p.name]) &&
+                    (form.errors[p.name] as string[])[i]) ||
+                    ''}
+                </FormErrorMessage>
+              </FormControl>
+            );
+          })}
+        </VStack>
       )}
     </FieldArray>
   );
 };
 
 export default MultiInput;
-
-// interface P {
-//   values: any[];
-//   label: string;
-//   countLabel: string;
-//   placeholder: string;
-//   name: string;
-//   type?: string;
-//   width?: string;
-//   isDisabled?: boolean;
-//   minAmountOfValues?: number;
-//   onChange: (
-//     value: string,
-//     index: number,
-//     e: React.ChangeEvent<HTMLInputElement>
-//   ) => void;
-//   onClickAdd: (value: string, index: number) => void;
-//   onClickRemove: (value: string, index: number) => void;
-// }
-
-// const MultiInput: React.FC<P> = (p) => {
-//   const values = (p.values || []).length === 0 ? [''] : p.values;
-//   const minAmountOfValues = p.minAmountOfValues || 0;
-
-//   return (
-//     <VStack gap={'13px'} alignItems="flex-start" width={p.width}>
-//       {values.map((v, i) => {
-//         return (
-//           <Input
-//             key={i}
-//             label={`${p.label} [${p.countLabel}${i + 1}] (integer)`}
-//             type={p.type || 'text'}
-//             name={`${p.name}[${i}]`}
-//             value={v.toString()}
-//             placeholder={p.placeholder}
-//             _placeholder={{
-//               fontSize: '14px',
-//             }}
-//             width={'100%'}
-//             isDisabled={p.isDisabled}
-//             onChange={(e) => p.onChange(v, i, e)}
-//             extra={
-//               <>
-//                 <HStack gap="8px">
-//                   {i > 0 && (
-//                     <S.IconButtonStyled
-//                       aria-label="remove value"
-//                       icon={<HiMinus />}
-//                       size="xs"
-//                       isDisabled={
-//                         values.length >= minAmountOfValues || p.isDisabled
-//                       }
-//                       onClick={() => {
-//                         p.onClickRemove(v, i);
-//                       }}
-//                     />
-//                   )}
-//                   {i === values.length - 1 && (
-//                     <S.IconButtonStyled
-//                       aria-label="Add value"
-//                       icon={<BsPlusLg />}
-//                       size="xs"
-//                       isDisabled={!values[i] || p.isDisabled}
-//                       onClick={() => p.onClickAdd(v, i)}
-//                     />
-//                   )}
-//                 </HStack>
-//               </>
-//             }
-//           />
-//         );
-//       })}
-//     </VStack>
-//   );
-// };
-
-// export default MultiInput;
