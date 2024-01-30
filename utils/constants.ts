d1fdc437b4932ba1da56165c1f203afc35af0a68
scottrepreneur <me@stwoh.com>
2024-01-30 16:33:50 -0600

    add sepolia, remove goerli

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,15 +1,14 @@
 import { Hex } from "viem";
 import {
-	goerli,
 	optimism,
 	mainnet,
 	gnosis,
-	// sepolia,
+	sepolia,
 	arbitrum,
 	polygon,
 } from "wagmi/chains";
 
-export type ChainKeys = 1 | 5 | 10 | 100 | 137 | 42161;
+export type ChainKeys = 1 | 10 | 100 | 137 | 42161 | 11155111;
 
 export const CONTRACTS: {
 	hatsSignerGateFactory: {
@@ -27,27 +26,27 @@ export const CONTRACTS: {
 } = {
 	hatsSignerGateFactory: {
 		1: "0x0F22eFC6EA47b1EFF42D1A2a6E69440929400F86",
-		5: "0xa1fb14b5f322651e20f06ee2f2681b8f380bbb0e",
 		10: "0x39Ae0B5e81A69F7092EC4394b111b6a6411377e8",
 		100: "0x0F22eFC6EA47b1EFF42D1A2a6E69440929400F86",
 		137: "0x0F22eFC6EA47b1EFF42D1A2a6E69440929400F86",
 		42161: "0x6FE52b46Af16F960E0eB1E15D2b27422ab158733",
+		11155111: "0x5CB8a5B063B7E94cF39E8A8813A777f49B8DD050",
 	},
 	hatsSignerGate: {
 		1: "0x19932b8E9dab073C0123a5792B44a8966Bf9C6eF",
-		5: "0x9B24db6ceB45F157B53d846f2e012C00956a67b6",
 		10: "0x19932b8E9dab073C0123a5792B44a8966Bf9C6eF",
 		100: "0x19932b8E9dab073C0123a5792B44a8966Bf9C6eF",
 		137: "0x19932b8E9dab073C0123a5792B44a8966Bf9C6eF",
 		42161: "0x9B24db6ceB45F157B53d846f2e012C00956a67b6",
+		11155111: "0x19932b8E9dab073C0123a5792B44a8966Bf9C6eF",
 	},
 	multiHatsSignerGate: {
 		1: "0x51486A1Db10992302922c5ae563088e1443d3d69",
-		5: "0xca2E8dC908Bb18ADaB496601f3FaEA3c69c19f6f",
 		10: "0x51486A1Db10992302922c5ae563088e1443d3d69",
 		100: "0x51486A1Db10992302922c5ae563088e1443d3d69",
 		137: "0x51486A1Db10992302922c5ae563088e1443d3d69",
 		42161: "0xca2E8dC908Bb18ADaB496601f3FaEA3c69c19f6f",
+		11155111: "0x51486A1Db10992302922c5ae563088e1443d3d69",
 	},
 	GnosisSafeL2: {
 		contractAddress: "0x3e5c63644e683549055b9be8653de26e0b4cd36e",
@@ -57,11 +56,11 @@ export const CONTRACTS: {
 // TODO check the other URLs here
 export const SAFE_API_URL: { [chainId in ChainKeys]: string } = {
 	1: "https://safe-transaction-mainnet.safe.global",
-	5: "https://safe-transaction-goerli.safe.global",
 	10: "https://safe-transaction-optimism.safe.global",
 	100: "https://safe-transaction-gnosis-chain.safe.global",
 	137: "https://safe-transaction-polygon.safe.global",
 	42161: "https://safe-transaction-arbitrum.safe.global",
+	11155111: "https://safe-transaction-sepolia.safe.global",
 };
 
 const customGnosis = {
@@ -78,8 +77,7 @@ export const SUPPORTED_NETWORKS = [
 	polygon,
 	customGnosis,
 	// testnets
-	goerli,
-	// sepolia,
+	sepolia,
 ];
 
 export enum DEPLOY_ACTIONS {
